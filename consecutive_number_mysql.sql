6c7e4041c0b4ea4bee644535688547809ee3e294
orangeka99 <mewmax99@gmail.com>
2026-09-20 02:40:39 +0700

    Change to subquery

--- consecutive_number_mysql.sql
+++ consecutive_number_mysql.sql
@@ -1,21 +1,18 @@
-WITH GroupedData AS (
-    SELECT
-        id,
-        num ,
-        CAST(ROW_NUMBER() OVER (ORDER BY id) AS SIGNED) 
-        - CAST(ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS SIGNED) AS group_id
-    FROM Logs
-)
-,Countingdata AS (
+WITH GroupedData AS ( 
 SELECT
     id,
     num,
     COUNT(*) OVER (PARTITION BY num, group_id) AS group_count
-FROM GroupedData
+FROM (SELECT
+        id,
+        num ,
+        CAST(ROW_NUMBER() OVER (ORDER BY id) AS SIGNED) 
+        - CAST(ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS SIGNED) AS group_id
+    FROM Logs ) AS Sub
 ORDER BY id ASC
 )
 SELECT num AS ConsecutiveNums 
-FROM Countingdata
+FROM GroupedData
 WHERE group_count > 2
 GROUP BY num
 
