6a0836c1a929e90d3725ba3975a5323dd260f870
orangeka99 <mewmax99@gmail.com>
2026-09-20 20:58:56 +0700

    Change To literaly Pandas

--- rank_scores_py.py
+++ rank_scores_py.py
@@ -1,22 +1,7 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    def_sort = scores['score'].sort_values(ascending=False)
-    prev_val = 0.00
-    my_dict = {'score':[],
-                'rank':[]
-                }
-    rank_id = 1
-    for i1 in range(len(def_sort)):
-        if i1 == 0:
-            prev_val = def_sort.iloc[i1]
-            my_dict.setdefault('score',[]).append(prev_val)
-            my_dict.setdefault('rank',[]).append(rank_id)
-        else:
-            if def_sort.iloc[i1] != prev_val:
-                rank_id += 1
-                prev_val = def_sort.iloc[i1]
-            my_dict.setdefault('score',[]).append(prev_val)
-            my_dict.setdefault('rank',[]).append(rank_id)
-
-    return pd.DataFrame(my_dict)
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False).astype(int)
+    scores = scores.sort_values(by='score', ascending=False)
+    return pd.DataFrame({'score': scores['score'],
+                         'rank' : scores['rank']})
