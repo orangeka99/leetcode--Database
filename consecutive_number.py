4c7be0067475c12e6aa32ebcf183947960e99675
orangeka99 <mewmax99@gmail.com>
2026-09-20 18:51:23 +0700

    180. Consecutive Numbers Pandas Alternatvie Fixed

--- consecutive_number.py
+++ consecutive_number.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
     s = logs['num']
-    mask = (s == s.shift(1)) & (s == s.shift(2))
-    return pd.DataFrame({'ConsecutiveNums': s[mask].unique()})
+    num_mat = (s == s.shift(1)) & (s == s.shift(2))
+    return pd.DataFrame({'ConsecutiveNums': s[num_mat].unique()})
