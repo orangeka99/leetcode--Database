59d5c2bf1b0f58c4bdcbd915482d7ec6ec67bbc3
orangeka99 <mewmax99@gmail.com>
2026-09-20 20:59:27 +0700

    Use assign()

--- rank_scores_py.py
+++ rank_scores_py.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
-    scores['rank'] = scores['score'].rank(method='dense', ascending=False).astype(int)
-    scores = scores.sort_values(by='score', ascending=False)
+    scores = scores.assign(rank=scores['score'].rank(method='dense', ascending=False).astype(int)
+    ).sort_values(by='score', ascending=False)
     return pd.DataFrame({'score': scores['score'],
                          'rank' : scores['rank']})
