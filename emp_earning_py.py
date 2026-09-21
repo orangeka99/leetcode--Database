e27e6c602af634e243f7ddfdcef6957f75bf1df7
orangeka99 <mewmax99@gmail.com>
2026-09-22 03:43:55 +0700

    181 Emp Pandas Fastest

--- emp_earning_py.py
+++ emp_earning_py.py
@@ -1,11 +1,6 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    left_emp = employee.sort_values('salary')
-    right_emp = employee.sort_values('salary')
     merge = pd.merge(employee, employee, left_on='managerId', right_on='id', how='left')
-    condition = (merge['salary_x'] > merge['salary_y'])
-    result = merge[condition]
-    # merge = pd.merge_asof(left_emp, right_emp, left_on='salary', right_on='salary', left_by='managerId', right_by='id', direction='forward',suffixes=('_left', '_right'))
-    # print(result)
+    result = merge[merge['salary_x'] > merge['salary_y']]
     return pd.DataFrame({'Employee':result['name_x']})
