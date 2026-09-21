39b81b75648507481d4f3112f634d32e60a9bc6f
orangeka99 <mewmax99@gmail.com>
2026-09-22 04:16:44 +0700

    dupl email 182 Pandas One line Alt 1

--- duplicate_emails_pandas.py
+++ duplicate_emails_pandas.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    person = person.sort_values(by='email', ascending=True)
-    person['count_email'] = person.groupby('email').cumcount() + 1
-    person = person[person['count_email'] == 2]
+    person = (
+        person.sort_values('email')
+        .assign(count_email=lambda df: df.groupby('email').cumcount() + 1)
+        .query('count_email == 2')
+    )
     return pd.DataFrame({'Email':person['email']})
